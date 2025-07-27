d5bff07aa05935d7f0f8756b005d472d427a04f8
Alejandro N. Cabral Valdovinos <87042690+alcabvaldo@users.noreply.github.com>
2025-07-27 15:54:34 -0300

    Revert "FEAT: AI Refactor to use Astro"

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://alcabvaldo.github.io',
-  base: '/',
-  output: 'static'
-});
